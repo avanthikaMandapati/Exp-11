15bf5792f500826bb6b8ae89eb7a2b3ac9e33d31
AVANTHIKA <AVANTHIKA@LAPTOP-KUNHLAJK>
2022-11-30 20:20:52 +0530

    Avanthika

---
+++ src/main/java/EExp_11/EExp_11/App.java
@@ -0,0 +1,18 @@
+package EExp_11.EExp_11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+        
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
---
+++ src/test/java/EExp_11/EExp_11/AppTest.java
@@ -0,0 +1,20 @@
+package EExp_11.EExp_11;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
